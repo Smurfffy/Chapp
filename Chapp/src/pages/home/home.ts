b38b52d8b5e6363ed9cd85c1c578a41660e0c828
Smurfffy <alanmurphy1995@gmail.com>
2017-02-04 14:06:09 +0000

    Added Login form with controles

--- Chapp/src/pages/home/home.ts
+++ Chapp/src/pages/home/home.ts
@@ -11,8 +11,10 @@ import { LoginPage } from '../login/login';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    window.localStorage.removeItem('currentuser');
     if(!this.isLoggedIn()){
       console.log('You are not logged in');
+      this.navCtrl.push(LoginPage);
     }
   }
 
