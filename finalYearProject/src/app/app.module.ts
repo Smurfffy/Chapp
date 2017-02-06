07037c258fdcb71549f0e6eb09d8676104517e0d
Smurfffy <alanmurphy1995@gmail.com>
2017-02-06 20:25:43 +0000

    Imported Pages into the app

--- finalYearProject/src/app/app.module.ts
+++ finalYearProject/src/app/app.module.ts
@@ -2,6 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { EventIndexPage } from '../pages/event-index/event-index';
+import { EventInfoPage } from '../pages/event-info/event-info';
+import { ForgotPasswordPage } from '../pages/forgot-password/forgot-password';
+import { LoginPage } from '../pages/login/login';
+import { MakeEventPage } from '../pages/make-event/make-event';
+import { ProfilePage } from '../pages/profile/profile';
+import { SignUpPage } from '../pages/sign-up/sign-up';
 
 @NgModule({
   declarations: [
