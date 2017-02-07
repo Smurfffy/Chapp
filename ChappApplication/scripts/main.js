34999b800ebd2efe2bc0236381a5eee3db27cdb5
Jason McTigue <jasonmctigue@live.ie>
2017-02-07 16:39:36 +0000

    added functions to fix sign in and out

--- ChappApplication/scripts/main.js
+++ ChappApplication/scripts/main.js
@@ -31,6 +31,7 @@ function Chapp() {
 
   
 }
+//========================================\\===============================================
 
 // Sets up shortcuts to Firebase features and initiate firebase authorization
 FriendlyChat.prototype.initFirebase = function() {
@@ -42,6 +43,7 @@ FriendlyChat.prototype.initFirebase = function() {
   this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
 };
 
+//========================================\\===============================================
 // Sign in Method for the application using the firebase credentials.
 Chapp.prototype.signIn = function() {
   // Sign in to Firebase using popup auth and Google as the identity provider(turn this on via firebase coonsole)
@@ -54,3 +56,67 @@ Chapp.prototype.signOut = function() {
    // Sign out of Firebase.
   this.auth.signOut();
 };
+
+//========================================\\===============================================
+
+//========================================\\===============================================
+
+// Triggers when the auth state change when the user signs-in or signs-out of the application.
+Chapp.prototype.onAuthStateChanged = function(user) {
+  if (user) { // User has sign in
+    // Get profile pic and user's name from the Firebase user object.
+    var profilePicUrl = null;  
+    var userName = null;//need to fix this
+
+    // Set the user's profile pic and name.
+    this.userPic.style.backgroundImage = 'url(' + profilePicUrl + ')';
+    this.userName.textContent = userName;
+
+    // Show user's profile and sign-out button.
+    this.userName.removeAttribute('hidden');
+    this.userPic.removeAttribute('hidden');
+    this.signOutButton.removeAttribute('hidden');
+
+    // Hide sign-in button.
+    this.signInButton.setAttribute('hidden', 'true');
+
+    // We load currently existing chat messages.
+    this.loadMessages();
+  } else { // User is signed out!
+    // Hide user's profile and sign-out button.
+    this.userName.setAttribute('hidden', 'true');
+    this.userPic.setAttribute('hidden', 'true');
+    this.signOutButton.setAttribute('hidden', 'true');
+
+    // Show sign-in button.
+    this.signInButton.removeAttribute('hidden');
+  }
+};
+
+// Returns true if user is signed-in. Otherwise false and displays a message.
+Chapp.prototype.checkSignedInWithMessage = function() {
+
+
+  // Display a message to the user
+  var data = {
+    message: 'You must sign-in first',
+    timeout: 2000
+  };
+  this.signInSnackbar.MaterialSnackbar.showSnackbar(data);
+  return false;
+};
+
+// Resets the given MaterialTextField.
+Chapp.resetMaterialTextfield = function(element) {
+  element.value = '';
+  element.parentNode.MaterialTextfield.boundUpdateClassesHandler();
+};
+
+//========================================\\===============================================
+// Template for messages.
+Chapp.MESSAGE_TEMPLATE =
+    '<div class="message-container">' +
+      '<div class="spacing"><div class="pic"></div></div>' +
+      '<div class="message"></div>' +
+      '<div class="name"></div>' +
+    '</div>';
