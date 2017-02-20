d54f127eec41d68102c9b735a386b74377d59c16
Smurfffy <alanmurphy1995@gmail.com>
2017-02-20 17:28:06 +0000

    Added Login function to login.ts

--- finalYearProject/src/pages/login/login.ts
+++ finalYearProject/src/pages/login/login.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, LoadingController, AlertController, NavParams } from 'ionic-angular';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Authentication } from '../../providers/authentication';
-import { HomePage } from '../home/home';
+import { HomePage } from '../../pages/home/home';
 import { SignUpPage } from '../../pages/sign-up/sign-up';
 import { ForgotPasswordPage } from '../../pages/forgot-password/forgot-password';
 import { EmailValidator } from '../../validators/email';
@@ -31,7 +31,7 @@ export class LoginPage {
     });
   }
 
-  goToResetPassword(){
+  goToForgetPassword(){
     this.nav.push(ForgotPasswordPage);
   }
 
@@ -45,6 +45,33 @@ export class LoginPage {
   }
 
   loginUsr() {
+    this.submitAttempt = true;
+
+    if(!this.loginForm.valid){
+      console.log(this.loginForm.value);
+    } else {
+      this.authData.loginUsr(this.loginForm.value.email, this.loginForm.value.password).then( authData => {
+        this.nav.setRoot(HomePage);
+    }, error => {
+      this.loading.dismiss().then( () => {
+        let alert = this.alertCntrl.create({
+          message: error.message,
+          buttons: [
+            {
+              text: "Ok",
+              role: 'cancel'
+            }
+          ]
+        });
+        alert.present();
+      });
+      });
+
+      this.loading = this.loadingCtrl.create({
+        dismissOnPageChange: true,
+      });
+      this.loading.present();
+    }
   }
 
   ionViewDidLoad() {
