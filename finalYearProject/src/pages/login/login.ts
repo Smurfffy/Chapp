430099ee187c02a7197463c7372af1d870f3b5d5
Smurfffy <alanmurphy1995@gmail.com>
2017-02-20 16:25:32 +0000

    Created file for email validation

--- finalYearProject/src/pages/login/login.ts
+++ finalYearProject/src/pages/login/login.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Authentication } from '../providers/authentication';
+import { HomePage } from '../home/home';
+import { SignUpPage } from '../pages/sign-up/sign-up';
+import { ForgotPasswordPage } from '../pages/forgot-password/forgot-password';
+import { EmailValidator } from '../validators/email';
 
 /*
   Generated class for the Login page.
