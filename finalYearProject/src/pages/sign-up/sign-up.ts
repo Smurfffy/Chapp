21398659ea8798cae1e2dbe9dca8bd7fa0ac16db
Smurfffy <alanmurphy1995@gmail.com>
2017-02-21 12:20:54 +0000

    Updated sign-up.ts with a create user function

--- finalYearProject/src/pages/sign-up/sign-up.ts
+++ finalYearProject/src/pages/sign-up/sign-up.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Authentication } from '../../providers/authentication';
+import { EmailValidator } from '../../validators/email';
+import { HomePage } from '../home/home';
 
 /*
   Generated class for the SignUp page.
@@ -12,8 +16,53 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'sign-up.html'
 })
 export class SignUpPage {
+  public signUpForm;
+  emailChanged: boolean = false;
+  passwordChanged: boolean = false;
+  submitAttempt: boolean = false;
+  loading;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public authentication: Authentication, public formBuilder: FormBuilder, public loadingCntrl: LoadingController, public alertCntrl: AlertController) {
+    this.signUpForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+      password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+    });
+  }
+
+  elementChanged(input) {
+    let field = input.inputControl.name;
+    this[field + "Changed"] = true;
+  }
+
+  signupUser() {
+        this.submitAttempt = true;
+
+        if (!this.signUpForm.valid){
+            console.log(this.signUpForm.value);
+        } else {
+            this.authentication.createUsr(this.signUpForm.value.email, 
+                this.signUpForm.value.password).then(() => {
+                  this.loading.dismiss().then( () =>{
+                    this.navCtrl.setRoot(HomePage);
+                  });
+                }, (error) => {
+                    this.loading.dismiss().then( () => {
+                        var errorMessage: string = error.message;
+                        let alert = this.alertCntrl.create({
+                            message: errorMessage,
+                            buttons: [{ text: "Ok", role: 'cancel' } ]
+                        });
+
+                        alert.present();
+                    });
+                });
+
+            this.loading = this.loadingCntrl.create({
+                dismissOnPageChange: true,
+            });
+            this.loading.present();
+        }
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignUpPage');
