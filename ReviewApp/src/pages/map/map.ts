d2c691097cb174bfc06cc07c5d3abbe9365ebe92
Jason McTigue <jasonmctigue@live.ie>
2017-03-14 12:43:37 +0000

    Added function to drop marker and see the distance from current location

--- ReviewApp/src/pages/map/map.ts
+++ ReviewApp/src/pages/map/map.ts
@@ -9,21 +9,31 @@ import { NavController, Platform } from 'ionic-angular';
 })
 export class MapPage {
  
-  @ViewChild('map') mapElement: ElementRef;
-  @ViewChild('pleaseConnect') pleaseConnect: ElementRef;
+    @ViewChild('map') mapElement: ElementRef;
+    @ViewChild('pleaseConnect') pleaseConnect: ElementRef;
  
-  constructor(public navCtrl: NavController, public maps: GoogleMaps, public platform: Platform, public locations: Locations) {
+    constructor(public navCtrl: NavController, public maps: GoogleMaps, public platform: Platform, public locations: Locations) {
  
-  }
+    }
  
-  ionViewDidLoad(){
+    ionViewDidLoad(){
  
-    this.platform.ready().then(() => {
+        this.platform.ready().then(() => {
  
-        let mapLoaded = this.maps.init(this.mapElement.nativeElement, this.pleaseConnect.nativeElement);
+            let mapLoaded = this.maps.init(this.mapElement.nativeElement, this.pleaseConnect.nativeElement);
+            let locationsLoaded = this.locations.load();
  
-    });
+            Promise.all([
+                mapLoaded,
+                locationsLoaded
+            ]).then((result) => {
  
-  }
+                let locations = result[1];
  
+                for(let location of locations){
+                    this.maps.addMarker(location.latitude, location.longitude);
+                }
+            });
+        });
+    }
 }
