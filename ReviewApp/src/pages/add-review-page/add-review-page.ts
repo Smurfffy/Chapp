389fff6349648917954f62cc15a244ec38cea3e5
Smurfffy <alanmurphy1995@gmail.com>
2017-04-08 15:56:31 +0100

    more comments

--- ReviewApp/src/pages/add-review-page/add-review-page.ts
+++ ReviewApp/src/pages/add-review-page/add-review-page.ts
@@ -1,3 +1,7 @@
+/*
+  This is the typescript file for the AddReview page in our app where users can add there own review item.
+*/
+
 import { Component } from '@angular/core';
 import { ViewController } from 'ionic-angular';
  
@@ -15,19 +19,22 @@ export class AddReviewPage {
  
   }
  
+ /*
+  The save() function saves a review to the mongo database provisioned to this app
+ */
   save(): void {
- 
-    let review = {
-      title: this.title,
-      description: this.description,
-      rating: this.rating
+    let review = { // the revew item
+      title: this.title, // the title of the review
+      description: this.description, // the review description
+      rating: this.rating // the review rating
     };
- 
-    this.viewCtrl.dismiss(review);
- 
+    this.viewCtrl.dismiss(review); // closes the add review page once added
   }
  
+ /*
+  The close() function closes the add review page when called
+ */
   close(): void {
-    this.viewCtrl.dismiss();
+    this.viewCtrl.dismiss(); // closes the add review page
   }
 }
--- ReviewApp/src/pages/home/home.ts
+++ ReviewApp/src/pages/home/home.ts
@@ -1,12 +1,14 @@
+/*
+  This is the home page that is navigated to once the user is loged in, it created the tabs for navigation and imports all the pages
+*/
+
 import { Component } from "@angular/core";
 import { NavController, ModalController } from 'ionic-angular';
-import { AddReviewPage } from '../add-review-page/add-review-page';
-import { Reviews } from '../../providers/reviews';
-
-//map imports
-import { MapPage } from '../map/map';
-import { ListPage } from '../list/list';
-import { ReviewPage } from '../review-list-page/review-list-page';
+import { AddReviewPage } from '../add-review-page/add-review-page'; // Adds the add review page to be loaded into a modal 
+import { Reviews } from '../../providers/reviews'; // Imports the provider
+import { MapPage } from '../map/map'; // imports the review page to be opened from a tab
+import { ListPage } from '../list/list'; // imports the list page to be opened from a tab
+import { ReviewPage } from '../review-list-page/review-list-page'; // imports the review page to be opened from a tab
  
 
 @Component({
@@ -15,30 +17,26 @@ import { ReviewPage } from '../review-list-page/review-list-page';
 })
 export class HomePage {
 
-  tab1Root: any = MapPage;
-  tab2Root: any = ListPage;
-  tab3Root: any = ReviewPage;
- 
-  
- 
-  reviews: any;
+  tab1Root: any = MapPage; // sets the map page to be in tab1
+  tab2Root: any = ListPage; // sets the list page to be in tab 2
+  tab3Root: any = ReviewPage; // sets the review page to be in tab 3
+  reviews: any; // review variable for creating and deleting reviews
  
   constructor(public nav: NavController, public reviewService: Reviews, public modalCtrl: ModalController) {
  
   }
  
   ionViewDidLoad(){
- 
+    // gets the reviews from the database
     this.reviewService.getReviews().then((data) => {
       console.log(data);
       this.reviews = data;
     });
- 
   }
  
   addReview(){
  
-    let modal = this.modalCtrl.create(AddReviewPage);
+    let modal = this.modalCtrl.create(AddReviewPage); // opens a modal with the add review page
  
     modal.onDidDismiss(review => {
       if(review){
@@ -46,22 +44,18 @@ export class HomePage {
         this.reviewService.createReview(review);        
       }
     });
- 
     modal.present();
- 
   }
  
   deleteReview(review){
  
-    //Remove locally
+    //Remove the review locally
       let index = this.reviews.indexOf(review);
  
       if(index > -1){
         this.reviews.splice(index, 1);
       }   
- 
-    //Remove from database
+    //Removes the review from the database
     this.reviewService.deleteReview(review._id);
   }
- 
 }
