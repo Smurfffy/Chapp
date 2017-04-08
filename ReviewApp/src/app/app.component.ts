b48c3882ad4e60f44a04730153ea176964e9f81a
Smurfffy <alanmurphy1995@gmail.com>
2017-04-08 12:45:08 +0100

    Added some comments

--- ReviewApp/src/app/app.component.ts
+++ ReviewApp/src/app/app.component.ts
@@ -2,19 +2,24 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
+/*
+  Instead of the home page we import the login page here so when the app starts the user is prompted to log in before the can 
+  start using the app.
+*/
 //import { HomePage } from '../pages/home/home';
-import { LoginScreen } from '../pages/loginscreen/loginscreen';
+import { LoginScreen } from '../pages/loginscreen/loginscreen'; // The login page is imported here
+
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = LoginScreen;
+  rootPage = LoginScreen; // This sets the LoginScreen page is the first page the user will see
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
+      // The platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
--- ReviewApp/src/app/app.module.ts
+++ ReviewApp/src/app/app.module.ts
@@ -1,24 +1,32 @@
 
+/*
+  This is our App.module.ts page. here we need to import all the compontents we created for our app.
+  It is important to import the components here otherwise we will not be able to use them in our app.
+  Any extra pages, providers, or npm packages we created or intalled are imported here.
+  We also connect to our firebase database for authentication here
+*/
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { AddReviewPage } from '../pages/add-review-page/add-review-page';
-import { Reviews } from '../providers/reviews';
-import { MapPage } from '../pages/map/map';
-import { ListPage } from '../pages/list/list';
-import { Locations } from '../providers/locations';
-import { GoogleMaps } from '../providers/google-maps';
-import { Connectivity } from '../providers/connectivity';
-import { ReviewPage } from '../pages/review-list-page/review-list-page';
- 
-import { AngularFireModule } from 'angularfire2';
-import { LoginScreen } from '../pages/loginscreen/loginscreen';
-import { AuthProvider} from '../providers/auth-provider'  //Added AuthProvider
-import { SignupPage } from '../pages/signup/signup'; //Added signup page
-import { ResetPasswordPage } from '../pages/reset-password/reset-password'; //Added reset password page
+import { AddReviewPage } from '../pages/add-review-page/add-review-page'; // Our add review page is imported here
+import { Reviews } from '../providers/reviews'; // Our provider for reviews is imported here
+import { MapPage } from '../pages/map/map'; // Our Google maps page is imported here
+import { ListPage } from '../pages/list/list'; // Our locations list page is imported here
+import { Locations } from '../providers/locations'; // Or provider for locations is imported here
+import { GoogleMaps } from '../providers/google-maps'; // Our Google maps provider page is imported here 
+import { Connectivity } from '../providers/connectivity'; // 
+import { ReviewPage } from '../pages/review-list-page/review-list-page'; // our reviews page is imported here
+import { AngularFireModule } from 'angularfire2'; // Angular fire is imported here to connect to our firebase database
+import { LoginScreen } from '../pages/loginscreen/loginscreen'; // our login page is imported here 
+import { AuthProvider} from '../providers/auth-provider'  // Our authentication provider is imported here 
+import { SignupPage } from '../pages/signup/signup'; // our signup page is imported here 
+import { ResetPasswordPage } from '../pages/reset-password/reset-password'; // Our reset password page is imported here.
 
 
+/*
+  Below we configure our app to connect to our firebase server which has the database for user authenticaion.
+*/
 export const firebaseConfig = {
   apiKey: "AIzaSyACh3rPmwkZ0c--dT58K3_K85LHMZIwf0c",
     authDomain: "ionic2loginpage.firebaseapp.com",
@@ -28,6 +36,9 @@ export const firebaseConfig = {
     messagingSenderId: "139153490469"
 };
 
+/*
+  now that everything is imported we declare the classes in the app here.
+*/
 @NgModule({
   declarations: [
     MyApp,
@@ -36,8 +47,8 @@ export const firebaseConfig = {
     MapPage,
     ListPage,
     ReviewPage,
-     SignupPage,
-     ResetPasswordPage, //Added signup page
+    SignupPage,
+    ResetPasswordPage,
     LoginScreen
   ],
   imports: [
